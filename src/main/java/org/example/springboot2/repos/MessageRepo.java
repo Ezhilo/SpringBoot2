8d134ffdc6e45cec4c01702a48496168d87ec36e
ezhilo <ezhilo@mail.ru>
2020-08-12 18:16:56 +0300

    срфзеук 4

---
+++ src/main/java/org/example/springboot2/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package org.example.springboot2.repos;
+
+import org.example.springboot2.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}
